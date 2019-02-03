0d9a687937f9915b9896b7a1f5015434456763dc
Ricky Raghavan <rajivsr@yahoo.com>
2019-02-03 15:32:23 -0500

    Configured Action to change the Redux Store

--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -1,7 +1,33 @@
 import {createStore} from 'redux';
 
-const store =createStore((state={count: 0})=>{
-return state;
+const store =createStore((state={count: 0},action)=>{
+    
+    switch(action.type){
+      case 'INCREMENT': 
+      console.log("Increment");
+      return{
+
+        count: state.count+1
+      };
+    case 'DECREMENT':
+    console.log("Decrement");
+    return{
+        count: state.count-1
+    };
+    case 'RESET':
+    console.log("Reset");
+    return {
+        count: 0
+    };
+    default: return state;
+    }
 });
+
+console.log(store.getState());
+
+store.dispatch({type: 'INCREMENT'});
+store.dispatch({type: 'INCREMENT'});
+store.dispatch({type: 'RESET'});
+store.dispatch({type: 'DECREMENT'});
 
 console.log(store.getState());
