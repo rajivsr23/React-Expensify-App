7ad337fe54670a550456b1c5d6dfab3082a4410f
Ricky Raghavan <rajivsr@yahoo.com>
2019-02-03 16:21:37 -0500

    Configured Dynamic Actions to change the Redux Store

--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -1,18 +1,25 @@
 import {createStore} from 'redux';
 
 const store =createStore((state={count: 0},action)=>{
-    
+ 
+  
     switch(action.type){
       case 'INCREMENT': 
+         const incrementBy = typeof action.incrementBy ==='number' ? action.incrementBy : 1
       console.log("Increment");
       return{
 
-        count: state.count+1
+        count: state.count+incrementBy
       };
     case 'DECREMENT':
     console.log("Decrement");
+      const decrementBy=typeof action.decrementBy ==='number'?action.decrementBy: 1
     return{
-        count: state.count-1
+        count: state.count-decrementBy
+    };
+    case 'SET':
+    return {
+        count: action.count
     };
     case 'RESET':
     console.log("Reset");
@@ -23,11 +30,28 @@ const store =createStore((state={count: 0},action)=>{
     }
 });
 
+
+
+const unsubscribe=store.subscribe(()=>{
+    
 console.log(store.getState());
 
+});
+
+store.dispatch({
+    type: 'INCREMENT',
+'incrementBy': 4});
 store.dispatch({type: 'INCREMENT'});
-store.dispatch({type: 'INCREMENT'});
+
 store.dispatch({type: 'RESET'});
 store.dispatch({type: 'DECREMENT'});
+store.dispatch({type: 'DECREMENT',
+decrementBy: 10});
 
-console.log(store.getState());
+store.dispatch({
+    type: 'SET',
+    count: 101
+});
+
+
+
