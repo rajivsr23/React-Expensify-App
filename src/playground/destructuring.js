2e56d493eb4590f426712ed86a84a31d4de7127b
Ricky Raghavan <rajivsr@yahoo.com>
2019-02-04 08:31:45 -0500

    ES6 Syntax for Destructuring Arrays

--- src/playground/destructuring.js
+++ src/playground/destructuring.js
@@ -1,10 +1,19 @@
-const book ={
-    title: 'Ego is the Enemy',
-    author: 'Ryan Holiday',
-    publisher: {
-        name: 'Penguin'
-    }
+//Object De-Structuring
+// const book ={
+//     title: 'Ego is the Enemy',
+//     author: 'Ryan Holiday',
+//     publisher: {
+//         name: 'Penguin'
+//     }
 
-};
-const {name: publisherName='Self-Published'}=book.publisher
-console.log(publisherName);
+// };
+// const {name: publisherName='Self-Published'}=book.publisher
+// console.log(publisherName);
+
+//Array De-Structuring
+
+const item=['Coffee (hot)','$2.00','$2.50','$2.75'];
+
+const [itemName,smallCoffee,mediumCoffee,largeCoffee]=item;
+
+console.log(`A medium ${itemName} costs ${mediumCoffee}`);
