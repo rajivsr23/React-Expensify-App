08c5e318631b18c229d5b799e84cdcfc5dc796b8
Ricky Raghavan <rajivsr@yahoo.com>
2019-02-13 07:52:14 -0500

    Remove Button to the ExpenseListItem Component and connected the component to the store

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,11 +1,21 @@
  import React from 'react';
-export default({description,amount,createdAt})=>(
+ import {removeExpense} from '../actions/expenses.js';
+ import {connect} from 'react-redux';
+const ExpenseListItem=({description,amount,createdAt,id,dispatch})=>(
 <div>
 <h3>{description} </h3>
 <p>Amount: {amount} </p>
 <p>Created At: {createdAt} </p>
+<button onClick={ ()=>{
+
+dispatch(removeExpense({id}));
+
+}}>Remove</button>
 </div>
 
 );
+
+export default connect()(ExpenseListItem);
+
 
 
