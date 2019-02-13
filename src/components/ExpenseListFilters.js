282caf25e622473380a0039536b3c835f0650686
Ricky Raghavan <rajivsr@yahoo.com>
2019-02-12 21:12:06 -0500

    Connected the ExpenseListFilters Component to the store

--- src/components/ExpenseListFilters.js
+++ src/components/ExpenseListFilters.js
@@ -1,8 +1,17 @@
 import React from 'react';
-
-export default(
+import {connect} from 'react-redux';
+const ExpenseListFilters=(props)=>(
 <div>
-<input type="text"/>
+<input type="text" value={props.filters.text}/>
 </div>
 
 );
+
+const mapStateToProps =(state)=>{
+return{
+filters: state.filters
+
+};
+};
+
+export default connect(mapStateToProps)(ExpenseListFilters);
