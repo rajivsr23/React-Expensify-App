22b732385e8bf55500af9751aec1db53d376338e
Ricky Raghavan <rajivsr@yahoo.com>
2019-02-14 22:20:51 -0500

    Added a State to Amount

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -6,7 +6,8 @@ export default class ExpenseForm extends React.Component{
     {
 
         description: '',
-        note:''
+        note:'',
+        amount: ''
     };
 onDescriptionChange=(e)=>{
 const description=e.target.value;
@@ -15,6 +16,14 @@ this.setState(()=>({description}));
 onNoteChange=(e)=>{
 const note=e.target.value;
 this.setState(()=>({note}));
+
+};
+
+onAmountChange=(e)=>{
+const amount =e.target.value;
+if(amount.match(/^\d*(\.\d{0,2})?$/)){
+    this.setState(()=>({amount}));
+}
 
 };
     render(){
@@ -31,8 +40,10 @@ this.setState(()=>({note}));
     onChange={this.onDescriptionChange}
     ></input><br/>
     <input
-    type="number"
+    type="text"
     placeholder="Amount"
+    value={this.state.amount}
+    onChange={this.onAmountChange}
     />
     <textarea
     placeholder="Add a note for your expense (optional)"
