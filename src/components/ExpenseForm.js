28510aeeaa854e50cf5be3c099b61f184c0d710b
Ricky Raghavan <rajivsr@yahoo.com>
2019-02-17 08:43:52 -0500

    Added Validation for Amount and Description

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -11,7 +11,8 @@ export default class ExpenseForm extends React.Component{
         note:'',
         amount: '',
         createdAt: moment(),
-        calendarFocused:false
+        calendarFocused:false,
+        error:''
     };
 onDescriptionChange=(e)=>{
 const description=e.target.value;
@@ -25,14 +26,18 @@ this.setState(()=>({note}));
 
 onAmountChange=(e)=>{
 const amount =e.target.value;
-if(amount.match(/^\d*(\.\d{0,2})?$/)){
+if(!amount||amount.match(/^\d{1,}(\.\d{0,2})?$/)){
     this.setState(()=>({amount}));
 }
 
 };
 
 onDateChange=(createdAt)=>{
-this.setState(()=>({createdAt}));
+    if(createdAt){
+
+        this.setState(()=>({createdAt}));
+    }
+
 
 };
 
@@ -40,12 +45,26 @@ onFocusChange=({focused})=>{
 this.setState(()=>({calendarFocused:focused}));
 };
 
+onSubmit=(e)=>{
+e.preventDefault();
+console.log("On Submit");
+if(!this.state.description || !this.state.amount){
+this.setState(()=>({error: 'Please provide description and amount.'}));
+}
+else{
+    this.setState(()=>({error:''}));
+    console.log('submitted');
+}
+};
+
+
     render(){
 
         return(
 
     <div> 
-    <form>
+    {this.state.error}
+    <form onSubmit={this.onSubmit}>
     <input 
     type="text"
     placeholder="description"
