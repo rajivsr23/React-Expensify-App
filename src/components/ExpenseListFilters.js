d7f06e3cf8dc0badf466248c01304fbf6622cf91
Ricky Raghavan <rajivsr@yahoo.com>
2019-02-18 08:25:44 -0500

    Implemented Date Filters

--- src/components/ExpenseListFilters.js
+++ src/components/ExpenseListFilters.js
@@ -2,23 +2,41 @@ import React from 'react';
 import {connect} from 'react-redux';
 import {setTextFilter} from '../actions/filters';
 import {sortByDate} from '../actions/filters';
-import {sortByAmount} from '../actions/filters';
-const ExpenseListFilters=(props)=>(
+import {sortByAmount,setStartDate,setEndDate} from '../actions/filters';
+import {DateRangePicker} from 'react-dates';
+
+class ExpenseListFilters extends React.Component{
+state={
+calendarFocused:null
+};
+onDatesChange=({startDate,endDate})=>{
+
+this.props.dispatch(setStartDate(startDate));
+this.props.dispatch(setEndDate(endDate));
+
+};
+
+onFocusChange=(calendarFocused)=>{
+this.setState(()=>({calendarFocused}));
+
+};
+    render(){
+        return(
 <div>
-<input type="text" value={props.filters.text} onChange={(e)=>{
+<input type="text" value={this.props.filters.text} onChange={(e)=>{
 
     props.dispatch(setTextFilter(e.target.value));
 }}
 
 />
 <select
-value={props.filters.sortBy}
+value={this.props.filters.sortBy}
 onChange={(e)=>{
 if(e.target.value==='date'){
-    props.dispatch(sortByDate());
+    this.props.dispatch(sortByDate());
 }
 else if(e.target.value==='amount'){
-    props.dispatch(sortByAmount());
+    this.props.dispatch(sortByAmount());
 }
 
 }}
@@ -26,10 +44,23 @@ else if(e.target.value==='amount'){
 <option value="date">Date</option>
 <option value="amount">Amount</option>
 </select>
-
+<DateRangePicker
+startDate={this.props.filters.startDate}
+endDate={this.props.filters.endDate}
+onDatesChange={this.onDatesChange}
+focusedInput={this.state.calendarFocused}
+onFocusChange={this.onFocusChange}
+showClearDates={true}
+numberOfMonths={1}
+isOutsideRange={()=>false}
+/>
 </div>
 
 );
+    }
+
+};
+
 
 const mapStateToProps =(state)=>{
 return{
--- src/selectors/expenses.js
+++ src/selectors/expenses.js
@@ -1,13 +1,14 @@
-
+import moment from 'moment';
 
 //First you start a project and then you create something. If the Items return true, it stays in the Array
 //Visible expenses
 export default (expenses,{text,sortBy,startDate,endDate})=>{
 return expenses.filter((expense)=>{
+    const createdAtMoment=moment(expense.createdAt);
     const description=expense.description.toLowerCase();
 const textMatch=expense.description.toLowerCase().includes(text.toLowerCase());
-const startDateMatch=typeof startDate !=='number'||expense.createdAt>=startDate;
-const endDateMatch=typeof endDate !=='number' || endDate>=expense.createdAt;
+const startDateMatch=startDate?startDate.isSameOrBefore(createdAtMoment,'day'):true;
+const endDateMatch= endDate?endDate.isSameOrBefore(createdAtMoment,'day') : true;
 
     return textMatch && startDateMatch && endDateMatch;
 
